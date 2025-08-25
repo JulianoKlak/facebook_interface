e82fb982514a71de1cd34f5082b5cad550f3b330
JulianoKlak <julianoklak@gmail.com>
2025-08-24 23:20:25 -0300

    adicionando área de postagem

--- lib/componentes/area_criar_postagem.dart
+++ lib/componentes/area_criar_postagem.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:facebook_interface/modelos/usuario.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 
 class AreaCriarPostagem extends StatelessWidget {
@@ -13,6 +14,67 @@ class AreaCriarPostagem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: EdgeInsets.fromLTRB(12, 8, 12, 8),
+      color: Colors.white,
+      child: Column(
+        children: [
+          Row(
+            children: [
+              CircleAvatar(
+                radius: 20,
+                backgroundColor: Colors.grey[200],
+                backgroundImage: CachedNetworkImageProvider(usuario.urlImagem),
+              ),
+              SizedBox(width: 8),
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration.collapsed(
+                    hintText: 'No que você está pensando?'
+              ))
+              )
+            ],
+          ),
+          Divider(
+            height: 10,
+            thickness: 0.5,
+          ),
+          Container(
+            height: 40,
+            child: Row(
+              children: [
+                TextButton.icon(
+                  onPressed: (){},
+                  icon: Icon(Icons.videocam, color: Colors.red,),
+                  label: Text(
+                    "Ao Vivo", style: TextStyle(
+                      color: Colors.black
+                    ) // textstyle
+                  ), //text
+                ), // textButton.icon
+                TextButton.icon(
+                  onPressed: (){},
+                  icon: Icon(Icons.photo_library, color: Colors.green,),
+                  label: Text(
+                    "Foto", style: TextStyle(
+                      color: Colors.black
+                    ) // textstyle
+                  ), //text
+                ), // textButton.icon
+                TextButton.icon(
+                  onPressed: (){},
+                  icon: Icon(Icons.video_call, color: Colors.purple,),
+                  label: Text(
+                    "Sala", style: TextStyle(
+                      color: Colors.black
+                    ) // textstyle
+                  ), //text
+                ), // textButton.icon
+              ]
+            ), // row
+          ) // container
+        ],
+      ),
+    );
   }
 }
